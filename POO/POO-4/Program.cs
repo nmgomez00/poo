117df78d4b6565b7855ae32058f16ede1bb085a7
Gomez <ngomez@FRSN.UTN.EDU.AR>
2024-07-31 17:47:47 -0300

    agregando mas cosas...

---
+++ POO/POO-4/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Empleado tiempo completo, empleado medio tiempo, nombre,salario,calcular salario()
+//
+//Console.WriteLine($"Salario de {radio.Marca}:{Muchisimo}"}
+//abstract class Empleado
